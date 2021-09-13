744173c73dce601a8d67ec74c4e63fb0d2e6a7ad
NordicBeaver <nordicbeaver@gmail.com>
2021-09-13 12:56:13 +0300

    Style header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+
+const HeaderStyled = styled.div`
+  padding: 1.4em 1.7em;
+  border-bottom: 2px solid #ffe8b0;
+`;
+
+const Logo = styled.span`
+  color: #ffe9b0;
+  font-size: 1.3em;
+`;
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderStyled>
+      <Logo>BeardyBear</Logo>
+    </HeaderStyled>
+  );
 }
