14e42342f211156fea623eaf53720a61c271c83b
NordicBeaver <nordicbeaver@gmail.com>
2021-09-13 12:24:20 +0300

    Create Header and Sidebar components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components/macro';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+const AppStyled = styled.div`
+  background-color: #141414;
+  color: #ffffff;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled>
+      <Header></Header>
+      <Sidebar></Sidebar>
+    </AppStyled>
   );
 }
 
