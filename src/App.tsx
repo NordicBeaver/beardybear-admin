d583f20bcf626b9194c4f6a7412a2d9e83894dae
NordicBeaver <nordicbeaver@gmail.com>
2021-09-13 13:39:48 +0300

    Style page layout.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,38 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
 const AppStyled = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 1fr 3fr;
   background-color: #141414;
   color: #ffffff;
+`;
+
+const HeaderContainer = styled.div`
+  grid-area: 1 / 1 / 2 / 3;
+`;
+
+const SidebarContainer = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
+  padding: 2em;
+`;
+
+const ContentContainer = styled.div`
+  grid-area: 2 / 2 / 3 / 3;
+  padding: 2em;
 `;
 
 function App() {
   return (
     <AppStyled>
-      <Header></Header>
-      <Sidebar></Sidebar>
+      <HeaderContainer>
+        <Header></Header>
+      </HeaderContainer>
+      <SidebarContainer>
+        <Sidebar></Sidebar>
+      </SidebarContainer>
+      <ContentContainer>Hi</ContentContainer>
     </AppStyled>
   );
 }
