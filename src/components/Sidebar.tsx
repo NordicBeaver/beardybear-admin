5f358791212630c0129280ac13364d9d6e6ec510
NordicBeaver <nordicbeaver@gmail.com>
2021-09-13 13:00:44 +0300

    Add Sidebar items and styles.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+
+const SidebarItem = styled.li`
+  font-size: 1.2em;
+  text-transform: uppercase;
+  margin-bottom: 1em;
+`;
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <ul>
+      <SidebarItem>Barbers</SidebarItem>
+      <SidebarItem>Services</SidebarItem>
+    </ul>
+  );
 }
