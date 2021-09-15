92411caf2739833dd16d3bcd0b636232e6837691
NordicBeaver <nordicbeaver@gmail.com>
2021-09-15 10:59:33 +0300

    An actual table for barbers.

--- src/components/BarbersTable.tsx
+++ src/components/BarbersTable.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 import { useGetBarbersQuery } from '../queries';
+
+const Table = styled.table`
+  width: 100%;
+  th {
+    text-align: start;
+    padding: 0.5em 1em 1em 0;
+  }
+  td {
+    padding: 0.5em 1em 0.5em 0;
+  }
+`;
 
 export default function BarbersTable() {
   const barbersQuery = useGetBarbersQuery();
 
+  if (!barbersQuery.isSuccess) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>{barbersQuery.status === 'success' ? barbersQuery.data.map((barber) => <div>{barber.name}</div>) : null}</div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {barbersQuery.data.map((barber) => (
+          <tr>
+            <td>{barber.name}</td>
+            <td>{barber.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
